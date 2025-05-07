81a478871cb03b3ea613017ca1531949590cd204
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-05-07 19:40:03 +0200

    feat: Add polish locale

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,14 +1,16 @@
 import en from '~~/i18n/en.json'
 import zh from '~~/i18n/zh.json'
 import nl from '~~/i18n/nl.json'
+import pl from '~~/i18n/pl.json'
 
 export default defineI18nConfig(() => ({
   legacy: false,
   fallbackLocale: 'zh',
-  availableLocales: ['en', 'zh', 'nl'],
+  availableLocales: ['en', 'zh', 'nl', 'pl'],
   messages: {
     en,
     zh,
     nl,
+    pl,
   },
 }))
